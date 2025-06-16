43a47066fb33aeb6c4262a9483f1e48fe215c627
mugabiBenjamin <mugabiben6@gmail.com>
2025-06-16 15:34:19 +0300

    Set navigator key in AuthProvider for improved navigation management

--- lib/app.dart
+++ lib/app.dart
@@ -22,6 +22,7 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    AuthProvider.setNavigatorKey(navigatorKey);
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => AuthProvider()),
