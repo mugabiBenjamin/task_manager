a6c54d3e553f77364062c09d4711c540ac69a6c1
mugabiBenjamin <mugabiben6@gmail.com>
2025-06-28 11:31:47 +0300

    Refactor EmailService: Update email sending logic to use HTTP requests

--- lib/services/email_service.dart
+++ lib/services/email_service.dart
@@ -1,19 +1,19 @@
-import 'package:emailjs/emailjs.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/foundation.dart';
 import '../models/task_model.dart';
 import '../core/utils/date_helper.dart';
 
 class EmailService {
-  static const String _serviceId = 'service_gevpfid';
-  static const String _templateId = 'template_77yrls5';
-  static const String _publicKey = 'NNbZWvJBb1rruB8eY';
-  static const String _invitationTemplateId = 'template_5p71bhu';
+  static const String _serviceId = 'service_e3qqbkr';
+  static const String _taskTemplateId = 'template_77yrls5';
+  static const String _invitationTemplateId = 'template_tifeuzq';
+  static const String _apiUrl = 'https://api.emailjs.com/api/v1.0/email/send';
 
   // Send task assignment notification
   static Future<bool> sendTaskAssignmentNotification({
     required TaskModel task,
-    required List<Map<String, dynamic>>
-    assignees, // Supports both registered and invited users
+    required List<Map<String, dynamic>> assignees,
     required Map<String, dynamic> creator,
   }) async {
     try {
@@ -24,42 +24,51 @@ class EmailService {
           continue;
         }
 
-        await send(
-          _serviceId,
-          _templateId,
-          {
-            'to_email': assignee['email'],
-            'to_name':
-                assignee['displayName'] ?? assignee['email'].split('@')[0],
-            'task_title': task.title,
-            'task_description': task.description,
-            'task_priority': task.priority.displayName,
-            'task_due_date': task.dueDate != null
-                ? DateHelper.formatDate(task.dueDate!)
-                : 'Not set',
-            'creator_name':
-                creator['displayName'] ?? creator['email'].split('@')[0],
-            'creator_email': creator['email'],
-            'task_link': 'https://task-manager-1763e.web.app/tasks/${task.id}',
-            'unsubscribe_link': assignee['isRegistered']
-                ? 'https://task-manager-1763e.web.app/unsubscribe/${assignee['id']}'
-                : null,
+        final templateParams = {
+          'to_name': assignee['displayName'] ?? assignee['email'].split('@')[0],
+          'to_email': assignee['email'],
+          'task_title': task.title,
+          'task_description': task.description,
+          'task_priority': task.priority.displayName,
+          'task_priority_class': task.priority.displayName.toLowerCase(),
+          'task_due_date': task.dueDate != null
+              ? DateHelper.formatDate(task.dueDate!)
+              : 'Not set',
+          'creator_name':
+              creator['displayName'] ?? creator['email'].split('@')[0],
+          'creator_email': creator['email'],
+          'task_link': 'https://task-manager-1763e.web.app/tasks/${task.id}',
+          'unsubscribe_link': assignee['isRegistered']
+              ? 'https://task-manager-1763e.web.app/unsubscribe/${assignee['id']}'
+              : '',
+        };
+
+        final response = await http.post(
+          Uri.parse(_apiUrl),
+          headers: {
+            'Content-Type': 'application/json',
+            'origin': 'http://localhost',
           },
-          const Options(
-            publicKey: _publicKey,
-            limitRate: LimitRate(
-              id: 'app_email_limit',
-              throttle: 10000, // 10 seconds between emails
-            ),
-          ),
+          body: json.encode({
+            'service_id': _serviceId,
+            'template_id': _taskTemplateId,
+            'template_params': templateParams,
+          }),
         );
+
+        if (response.statusCode != 200) {
+          throw Exception('Failed to send email: ${response.body}');
+        }
+
+        // Add delay between emails to respect rate limiting
+        await Future.delayed(const Duration(seconds: 2));
       }
       return true;
     } catch (e) {
       if (kDebugMode) {
         print('Email notification failed: $e');
       }
-      return false; // Don't block task assignment on email failure
+      return false;
     }
   }
 
@@ -72,25 +81,32 @@ class EmailService {
     required String verificationLink,
   }) async {
     try {
-      await send(
-        _serviceId,
-        _invitationTemplateId,
-        {
-          'to_email': recipientEmail,
-          'to_name': recipientEmail.split('@')[0],
-          'inviter_name': inviterName,
-          'inviter_email': inviterEmail,
-          'invitation_link': verificationLink,
-          'app_name': 'Task Manager',
+      final templateParams = {
+        'to_name': recipientEmail.split('@')[0],
+        'to_email': recipientEmail,
+        'inviter_name': inviterName,
+        'inviter_email': inviterEmail,
+        'invitation_link': verificationLink,
+        'app_name': 'Task Manager',
+      };
+
+      final response = await http.post(
+        Uri.parse(_apiUrl),
+        headers: {
+          'Content-Type': 'application/json',
+          'origin': 'http://localhost',
         },
-        const Options(
-          publicKey: _publicKey,
-          limitRate: LimitRate(
-            id: 'invitation_email_limit',
-            throttle: 15000, // 15 seconds between invitation emails
-          ),
-        ),
+        body: json.encode({
+          'service_id': _serviceId,
+          'template_id': _invitationTemplateId,
+          'template_params': templateParams,
+        }),
       );
+
+      if (response.statusCode != 200) {
+        throw Exception('Failed to send invitation: ${response.body}');
+      }
+
       return true;
     } catch (e) {
       if (kDebugMode) {
