2af28b3af1e763e541a7edce0060cf38185eb4b3
mugabiBenjamin <mugabiben6@gmail.com>
2025-06-30 12:42:07 +0300

    Refactor App class: Convert to StatefulWidget and add AcceptInvitationScreen route

--- lib/app.dart
+++ lib/app.dart
@@ -8,6 +8,7 @@ import 'providers/user_provider.dart';
 import 'routes/app_routes.dart';
 import 'screens/auth/login_screen.dart';
 import 'screens/auth/signup_screen.dart';
+import 'screens/auth/accept_invitation_screen.dart';
 import 'screens/profile/user_profile_screen.dart';
 import 'screens/settings/settings_screen.dart';
 import 'screens/tasks/create_task_screen.dart';
@@ -18,15 +19,25 @@ import 'screens/tasks/task_details_screen.dart';
 import 'screens/tasks/task_list_screen.dart';
 import 'screens/auth/invite_user_screen.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
 
   static final GlobalKey<NavigatorState> navigatorKey =
       GlobalKey<NavigatorState>();
 
   @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    AuthProvider.setNavigatorKey(navigatorKey);
+    AuthProvider.setNavigatorKey(App.navigatorKey);
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => AuthProvider()),
@@ -47,7 +58,7 @@ class App extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Task Manager',
         theme: AppTheme.lightTheme,
-        navigatorKey: navigatorKey,
+        navigatorKey: App.navigatorKey,
         initialRoute: AppRoutes.login,
         routes: {
           AppRoutes.login: (context) => const LoginScreen(),
@@ -70,8 +81,11 @@ class App extends StatelessWidget {
           AppRoutes.settings: (context) => const SettingsScreen(),
           AppRoutes.starredTasks: (context) => const StarredTasksScreen(),
           AppRoutes.labels: (context) => const LabelsScreen(),
-          AppRoutes.inviteUser: (context) =>
-              const InviteUserScreen(),
+          AppRoutes.inviteUser: (context) => const InviteUserScreen(),
+          AppRoutes.acceptInvitation: (context) {
+            final token = ModalRoute.of(context)!.settings.arguments as String;
+            return AcceptInvitationScreen(token: token);
+          },
         },
       ),
     );
